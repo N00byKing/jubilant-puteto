40434062814ecd6a04b3d6259aca3f12f43519f3
N00byKing <N00byKing@users.noreply.github.com>
2018-03-26 19:35:59 +0200

    Update javadoc_update.sh

--- .travis/javadoc_update.sh
+++ .travis/javadoc_update.sh
@@ -6,19 +6,18 @@ if [ "$TRAVIS_REPO_SLUG" == "N00byKing/jubilant-puteto" ] && [ "$TRAVIS_JDK_VERS
   git config --global user.name "travis-ci"
   git clone https://${GH_TOKEN}@github.com/N00byKing/jubilant-puteto-doc
 
-  rm -rf ./jubilant-puteto-doc
-
   echo -e "Generating javadoc...\n"
-  
-  javadoc $(find . -name *.java) -d jubilant-puteto-doc/ 
+
+  javadoc -sourcepath ${pwd} -d jubilant-puteto-doc/ -subpackages .
 
   echo -e "Publishing javadoc...\n"
 
+  cd jubilant-puteto-doc
   git status
   git add *
   git commit -m "Latest javadoc for commit $TRAVIS_COMMIT auto-pushed to gh-pages"
   git push -f
 
   echo -e "Javadoc Updated.\n"
-  
+
 fi
